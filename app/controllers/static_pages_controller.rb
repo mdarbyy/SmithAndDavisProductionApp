9fe5578916acc6fa5475bbe521d8be3d8dda4e2e
mdarbyy <michaeldarby2698@gmail.com>
2025-11-11 20:33:42 -0500

    Built Dashboard Page Design
    moved over the code that creates the design of the dashboard page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def dashboard
+
+    if params[:start_date].blank? || params[:end_date].blank?
+      return
+    end
+
+    @start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
+    @end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
   end
 end
